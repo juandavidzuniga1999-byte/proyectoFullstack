791a270021f06f3045b715feb05fbeeb90f11ba3
juandavidzuniga1999-byte <juan.david.zuniga.1999@gmail.com>
2026-09-17 14:43:48 -0600

    agregado de service

---
+++ src/main/java/com/guardado/proyecto/service/propiedadService.java
@@ -0,0 +1,13 @@
+package com.guardado.proyecto.service;
+
+import org.springframework.stereotype.Service;
+
+import com.guardado.proyecto.model.Propiedad;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service 
+public class propiedadService {
+    private List<Propiedad> propiedades=new ArrayList<>();
+}
